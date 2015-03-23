aa0aa209f5dfd1f6f24ffdcc9302455747abd035
Aleksuk <aleksuk.m@gmail.com>
2015-03-24 00:03:55 +0200

    Comments was removed

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,6 @@
 class Order < ActiveRecord::Base
+
   has_many :products, through: :order_products 
   has_many :order_products
-
-
-
-
-  # validate :my_method
-
-  # def my_method
-  #   if name.length = 1 then self.errors.add(:name, 'Bad name')
-  # end
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ class Product < ActiveRecord::Base
   has_many :orders, through: :order_products
   has_many :order_products
 
-  # validate :name, presence: true
   validates :name,
             presence: true,
             length: { minimum: 2 }
@@ -11,7 +10,6 @@ class Product < ActiveRecord::Base
   validates :price,
             presence: true,
             numericality: true
-  # validate :my_validation, on: :name
 
   def convert_to_ua
     price * 8
